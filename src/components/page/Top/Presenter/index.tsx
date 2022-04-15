cd3d074ee63dc70ffd5036f8de75429c3e6b4864
kai815 <h.kaizuka815@gmail.com>
2022-04-15 16:26:44 +0900

    検索ボタンやinputの配置

--- src/components/page/Top/Presenter/index.tsx
+++ src/components/page/Top/Presenter/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
-import { Heading, Box } from '@chakra-ui/react';
+import { Heading, Box, Input, Button, Stack, HStack } from '@chakra-ui/react';
 
 //todo後で必要になるデータを定義する
 // type Props = {};
@@ -13,12 +13,18 @@ const TopPagePresenter = () => (
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
     {/* スマホのみのサイズを対応する予定なので、500pxにしてる */}
-    <Box maxW='500px' minH='100vh' marginX='auto' boxShadow='base'>
-      <Heading color='red'>お天気アプリ</Heading>
-      <footer>
-        <hr />
-        <span>お天気フッター</span>
-      </footer>
+    <Box maxW='500px' minH='100vh' marginX='auto' boxShadow='base' padding={4}>
+      <Heading as='h1'>お天気チェック</Heading>
+      <Stack spacing={4} marginY={5}>
+        <Input variant='outline' placeholder='緯度' />
+        <Input variant='outline' placeholder='経度' />
+        <Button colorScheme='orange' ml='auto'>
+          検索
+        </Button>
+      </Stack>
+      <Heading as='h2' textAlign='center'>
+        日付
+      </Heading>
     </Box>
   </div>
 );
