2474beeaa1bc1737bd48994c2c6e9b7beb39e4d8
kai815 <h.kaizuka815@gmail.com>
2022-04-15 19:34:48 +0900

    試しにswrでデータ取得

--- src/components/page/Top/Container/index.tsx
+++ src/components/page/Top/Container/index.tsx
@@ -1,11 +1,27 @@
-import React, { ReactNode } from 'react';
-import Head from 'next/head';
-import { Heading } from '@chakra-ui/react';
+import React from 'react';
+import useSWR from 'swr';
 import TopPagePresenter from '../Presenter';
 
-//todo後で必要になるデータを定義する
-// type Props = {};
+const fetcher = (url: string): Promise<any> =>
+  fetch(url).then((res) => res.json());
+
+type WeatherListResponse = {
+  weather: {
+    icon: string;
+    code: string;
+    description: string;
+  };
+  max_temp: number;
+  min_temp: number;
+};
+
 //Containerはデータ取得してPresentatorに渡す
-const TopPageContainer = () => <TopPagePresenter />;
+const TopPageContainer = () => {
+  const { data, error } = useSWR<WeatherListResponse>(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}daily?lat=30&lon=130&days=7&key=${process.env.NEXT_PUBLIC_API_KEY}`,
+    fetcher,
+  );
+  return <TopPagePresenter />;
+};
 
 export default TopPageContainer;
