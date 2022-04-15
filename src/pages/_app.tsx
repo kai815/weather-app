29e69faf9520f5db4e9006badfb5845932a567b9
kai815 <h.kaizuka815@gmail.com>
2022-04-15 15:09:40 +0900

    yarn format コマンドの事項

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
