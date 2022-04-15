21e1195475e1675bc8b3a1e534c8bf0117deb282
kai815 <h.kaizuka815@gmail.com>
2022-04-15 16:45:41 +0900

    最低限のレイアウト

--- src/components/page/Top/Presenter/index.tsx
+++ src/components/page/Top/Presenter/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
-import { Heading, Box, Input, Button, Stack, HStack } from '@chakra-ui/react';
+import { Heading, Box, Input, Button, VStack, HStack } from '@chakra-ui/react';
+import WeatherCard from '../../../ui/WeatherCard';
 
 //todo後で必要になるデータを定義する
 // type Props = {};
@@ -15,16 +16,25 @@ const TopPagePresenter = () => (
     {/* スマホのみのサイズを対応する予定なので、500pxにしてる */}
     <Box maxW='500px' minH='100vh' marginX='auto' boxShadow='base' padding={4}>
       <Heading as='h1'>お天気チェック</Heading>
-      <Stack spacing={4} marginY={5}>
+      <VStack spacing={4} marginY={5}>
         <Input variant='outline' placeholder='緯度' />
         <Input variant='outline' placeholder='経度' />
         <Button colorScheme='orange' ml='auto'>
           検索
         </Button>
-      </Stack>
-      <Heading as='h2' textAlign='center'>
+      </VStack>
+      <Heading as='h2' textAlign='center' mb='2'>
         日付
       </Heading>
+      <WeatherCard />
+      <HStack justifyContent='space-between' mt='6'>
+        <Button variant='outline' colorScheme='blue'>
+          前日
+        </Button>
+        <Button variant='outline' colorScheme='blue'>
+          翌日
+        </Button>
+      </HStack>
     </Box>
   </div>
 );
