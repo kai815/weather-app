b239e30847286d8db638b55c26e458cb2e8d7296
kai815 <h.kaizuka815@gmail.com>
2022-04-15 16:16:50 +0900

    外側のレイアウト

--- src/components/page/Top/Presenter/index.tsx
+++ src/components/page/Top/Presenter/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Box } from '@chakra-ui/react';
 
 //todo後で必要になるデータを定義する
 // type Props = {};
@@ -12,11 +12,14 @@ const TopPagePresenter = () => (
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
-    <Heading color='red'>お天気アプリ</Heading>
-    <footer>
-      <hr />
-      <span>お天気フッター</span>
-    </footer>
+    {/* スマホのみのサイズを対応する予定なので、500pxにしてる */}
+    <Box maxW='500px' minH='100vh' marginX='auto' boxShadow='base'>
+      <Heading color='red'>お天気アプリ</Heading>
+      <footer>
+        <hr />
+        <span>お天気フッター</span>
+      </footer>
+    </Box>
   </div>
 );
 
